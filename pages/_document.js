2c3b8edd48bfe7f328c23684f94a85d2e49d49d8
Smiletech3D <contato@smiletech3d.com.br>
2026-01-01 23:15:00 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="pt-BR">
-      <Head />
+      <Head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.png" type="image/png" />
+
+        {/* (Opcional) compatibilidade extra */}
+        <link rel="apple-touch-icon" href="/favicon.png" />
+        <meta name="theme-color" content="#f59e0b" />
+      </Head>
       <body>
         <Main />
         <NextScript />
